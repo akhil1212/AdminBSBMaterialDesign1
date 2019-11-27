dd8ab72a746c45c091bc6f440bcfeeefe3cc272d
akhil1212 <akhilgelli1141@gmail.com>
2019-11-27 11:34:28 +0530

    Added the code

---
+++ pages/examples/assets/js/main_bar.js
@@ -0,0 +1,17 @@
+var exampleData = {
+  'Custom Code Adaption': 28,
+  'User Traning': 27,
+  'Customize': 25,
+  'Others': 87
+};
+
+var exampleOptions = {
+  'height': 350,
+  'title': '',
+  'width': 350,
+  'fixPadding': 18,
+  'barFont': [0, 12, "bold"],
+    'labelFont': [0, 13, 0]
+};
+
+graphite(exampleData, exampleOptions, example);
